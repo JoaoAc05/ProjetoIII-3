45a42017349c252b972005817713c58262404498
João Alves <joaoalvescordeiro2005@gmail.com>
2024-10-25 17:24:10 -0400

    ajuste rota

--- Routes/ChamadaAlunosRouter.js
+++ Routes/ChamadaAlunosRouter.js
@@ -8,6 +8,6 @@ ChamadaAlunosRouter.get('/alunos', ChamadaAlunosController.getAll);
 ChamadaAlunosRouter.get('/:id_chamada/alunos', ChamadaAlunosController.getId); // Get pelo ID da chamada
 ChamadaAlunosRouter.post('/alunos', ChamadaAlunosController.presenca); 
 ChamadaAlunosRouter.put('/alunos', ChamadaAlunosController.alterar); 
-ChamadaAlunosRouter.delete(':id_chamada/alunos/:id_aluno', ChamadaAlunosController.deletar); // Delete pelo ID do aluno
+ChamadaAlunosRouter.delete('/:id_chamada/alunos/:id_aluno', ChamadaAlunosController.deletar); // Delete pelo ID do aluno
 
 export { ChamadaAlunosRouter };
